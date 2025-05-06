feb43941409cd7ffd70bca8b7ce936ee8d1c7ef2
RelaxHub <89567097+RadientScript@users.noreply.github.com>
2025-05-07 03:17:28 +0700

    Update Join.lua

--- Join.lua
+++ Join.lua
@@ -1,47 +1,33 @@
-local inLobby = game.PlaceId == 12886143095 or game.PlaceId == 18583778121
-local character = game.Players.LocalPlayer.Character or game.Players.LocalPlayer.CharacterAdded:Wait();local humanoid = character:FindFirstChildOfClass("Humanoid");local rootPart = character:WaitForChild("HumanoidRootPart")
-local targetName = "bohqlw"
+repeat task.wait() until game:IsLoaded() and game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui"):WaitForChild("MainUI") and game:GetService("Players").LocalPlayer.PlayerGui:WaitForChild("MainUI").Enabled
 
-repeat
-    foundPlayer = game:GetService("Players"):FindFirstChild(targetName) 
-    if inLobby then
-        writefile(tostring(game.Players.LocalPlayer.UserId).."JobId.txt",game.JobId)
-        local MainJobId = readfile("5654706557JobId.txt")
-        if MainJobId ~= game.JobId then
-            game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, MainJobId)
-        end
+local url = "https://discord.com/api/webhooks/1230899982165479455/Zx3pyf5_DuGB-FvftwuyTs1f4F16G9mby0hHEKbkUPFBwMIxV14mQp4Nwhvtzuj2LCQK"
+local matchCounter = 0
+local afterMatch = 2
+local previousState = false
+local h=game:GetService("HttpService") local r=http_request or syn.request function s(u,m)r({Url=u,Method="POST",Headers={["Content-Type"]="application/json"},Body=h:JSONEncode({content=m})})end
+
+local function GameEnded()
+    return game.Players.LocalPlayer.PlayerGui:FindFirstChild("EndGameUI") ~= nil
+end
+
+print("Waiting for ".. afterMatch .." matches...");s(url, "Join Wait ".. afterMatch .." match.")
+game:GetService("StarterGui"):SetCore("SendNotification", {Title = "Start Count Restart",Text = "Restart Working!!",Duration = 9e9})
+while matchCounter < afterMatch do
+    local currentState = GameEnded()
+
+    if currentState and not previousState then
+        matchCounter = matchCounter + 1
+        print("Game ended! Count :", matchCounter);s(url, matchCounter .. " matches played.");game:GetService("StarterGui"):SetCore("SendNotification", {Title = "Restart Working!!",Text = tostring(matchCounter).." matches",Duration = 9e9})
     end
-    task.wait(1) 
-until foundPlayer
 
-repeat task.wait()
-    if game.Players.LocalPlayer.UserId == 5654706557 then
-        for i,v in pairs(workspace.TeleporterFolder.Story:GetChildren()) do
-            if game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("TeleportUI") and game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("TeleportUI").Enabled then
-                if v:GetAttribute("Leader") == game.Players.LocalPlayer.UserId and v:GetAttribute("PlayerCount") >= 2 then
-                    game:GetService("GuiService").SelectedObject = game:GetService("Players").LocalPlayer.PlayerGui.TeleportUI.Skip
-                    game:GetService("VirtualInputManager"):SendKeyEvent(true, Enum.KeyCode.Return, false, game)
-                    game:GetService("VirtualInputManager"):SendKeyEvent(false, Enum.KeyCode.Return, false, game)
-                    task.wait(1)
-                end
-            elseif game:GetService("Players").LocalPlayer.PlayerGui.Story.Enabled then
-                    local args = {
-                    [1] = "Thriller Bark",
-                    [2] = "Infinite",
-                    [3] = "Nightmare",
-                    [4] = true --friend
-                }
+    previousState = currentState
 
-                game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Story"):WaitForChild("Select"):InvokeServer(unpack(args))
-            elseif v:FindFirstChild("Door") and not v:GetAttribute("Leader") then
-                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.Door.CFrame;humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
-            end
-        end
-    elseif game.Players.LocalPlayer.UserId == 3963988619 then
-        for i,v in pairs(workspace.TeleporterFolder.Story:GetChildren()) do
-            if v:GetAttribute("Leader") == 5654706557 and v:GetAttribute("Map") then
-                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.Door.CFrame;humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
-            end
-        end
-    end
-until not inLobby
+	task.wait(1)
+end
+
+while task.wait(1) do
+	if not game.Players.LocalPlayer.PlayerGui:FindFirstChild("EndGameUI") then
+		game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RestartMatch"):FireServer()
+		print("restart");s(url, "restart. @everyone")
+	end
+end
