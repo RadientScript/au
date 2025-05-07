658f53f2143abe920f3a15ea08b0b5d56e508d78
RelaxHub <89567097+RadientScript@users.noreply.github.com>
2025-05-07 15:40:41 +0700

    Update Join.lua

--- Join.lua
+++ Join.lua
@@ -11,6 +11,7 @@ local function GameEnded()
     return game.Players.LocalPlayer.PlayerGui:FindFirstChild("EndGameUI") ~= nil
 end
 
+print("Version 1.0.0")
 print("Waiting for ".. getgenv().Match .." matches...");s(getgenv().WebhookURL, "Join Wait ".. getgenv().Match .." match.")
 game:GetService("StarterGui"):SetCore("SendNotification", {Title = "Start Count Restart",Text = "Restart Working!!",Duration = 9e9})
 while matchCounter < getgenv().Match do
