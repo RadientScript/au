0ab1aa91f02133e5177fa3f15d8d5b5762683f80
RelaxHub <89567097+RadientScript@users.noreply.github.com>
2025-05-08 02:08:50 +0700

    Update Join.lua

--- Join.lua
+++ Join.lua
@@ -1,7 +1,12 @@
 repeat task.wait() until game:IsLoaded() and game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui"):WaitForChild("MainUI") and game:GetService("Players").LocalPlayer.PlayerGui:WaitForChild("MainUI").Enabled
 
-if not getgenv().Match then getgenv().Match = 4 end
-if not getgenv().WebhookURL then getgenv().WebhookURL = "" end
+if not getgenv().Main then
+	getgenv().Main = {
+		["Mode"] = "Restart", --"Restart","Leave"
+		["Attempts"] = 4,
+		["WebhookURL"] = ""
+	}
+end
 
 local matchCounter = 0
 local previousState = false
@@ -11,14 +16,14 @@ local function GameEnded()
     return game.Players.LocalPlayer.PlayerGui:FindFirstChild("EndGameUI") ~= nil
 end
 
-print("Waiting for ".. getgenv().Match .." matches...");s(getgenv().WebhookURL, "Join Wait ".. getgenv().Match .." match.")
+print("Waiting for ".. getgenv().Main["Attempts"] .." matches...");s(getgenv().Main["WebhookURL"], "Join Wait ".. getgenv().Main["Attempts"] .." match.")
 game:GetService("StarterGui"):SetCore("SendNotification", {Title = "Start Count Restart",Text = "Restart Working!!",Duration = 9e9})
-while matchCounter < getgenv().Match do
+while matchCounter < getgenv().Main["Attempts"] do
     local currentState = GameEnded()
 
     if currentState and not previousState then
         matchCounter = matchCounter + 1
-        print("Game ended! Count :", matchCounter);s(getgenv().WebhookURL, matchCounter .. " matches played.");game:GetService("StarterGui"):SetCore("SendNotification", {Title = "Restart Working!!",Text = tostring(matchCounter).." matches",Duration = 9e9})
+        print("Game ended! Count :", matchCounter);s(getgenv().Main["WebhookURL"], matchCounter .. " matches played.");game:GetService("StarterGui"):SetCore("SendNotification", {Title = "Restart Working!!",Text = tostring(matchCounter).." matches",Duration = 9e9})
     end
 
     previousState = currentState
@@ -28,7 +33,12 @@ end
 
 while task.wait(1) do
 	if not game.Players.LocalPlayer.PlayerGui:FindFirstChild("EndGameUI") then
-		game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RestartMatch"):FireServer()
-		print("restart");s(getgenv().WebhookURL, "restart. @everyone")
+        if getgenv().Main["Mode"] == "Restart" then
+            game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RestartMatch"):FireServer()
+            print("restart!");s(getgenv().Main["WebhookURL"], "restart. @everyone")
+        elseif getgenv().Main["Mode"] == "Leave" then
+            game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("TeleportBack"):FireServer()
+            print("leave!");s(getgenv().Main["WebhookURL"], "leave. @everyone")
+        end
 	end
 end
